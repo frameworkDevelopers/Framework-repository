d9ac1d433ec0092f9f7ce7254fd8a4b2bb9efe53
Ankit <ankit.patil@ikione.com>
2024-01-04 20:27:41 +0530

    basic theme added

--- Frontend/react-framework/src/App.js
+++ Frontend/react-framework/src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import theme from './Common/Theme';
 import './App.css';
 
-function App() {
+const App = () => {
+  const appStyles = {
+    background:theme.pageBackground.secondary, 
+    borderRadius: theme.borderRadius.medium, 
+    margin:theme.space.large
+  };
+
+  const listItemStyles = {
+  
+  };
+
+  const dummyData = [
+    { id: 1, title: 'Item 1', description: 'Description for Item 1' },
+    { id: 2, title: 'Item 2', description: 'Description for Item 2' },
+    { id: 3, title: 'Item 3', description: 'Description for Item 3' },
+  ];
+
   return (
-    <div className="App">
-    <h1>hi</h1>
+    <div className="App" style={appStyles}>
+      <h1>My App</h1>
+
+        {dummyData.map(({ id, title, description }) => (
+          <div key={id} style={{display:'flex', justifyContent:'space-around'}}>
+            <h2 style={{ fontSize: theme.fontSize.large }}>{title}</h2>
+            <p style={{ fontSize: theme.fontSize.medium }}>{description}</p>
+          </div>
+        ))}
+
     </div>
   );
-}
+};
 
 export default App;
---
+++ Frontend/react-framework/src/Common/Theme.js
@@ -0,0 +1,74 @@
+const theme = {
+  // Spaces for margin, padding
+  space: {
+    small: 2,
+    medium: 8,
+    large: 12,
+  },
+
+  // Font sizes
+  fontSize: {
+    small: 12,
+    medium: 16,
+    large: 20,
+  },
+
+  // Colors
+  color: {
+    primary: "#191970",
+    secondary: "#C0C0C0",
+    tertiary: "#008080",
+    white: "#F2F7FC",
+    text: "#222222",
+    black: "#222222",
+    success: "#4CAF50",
+    error: "#FF0000",
+    warning: "#FFA500",
+  },
+
+  // Border Radius
+  borderRadius: {
+    small: 4,
+    medium: 8,
+    large: 12,
+  },
+
+  // Button Styles
+  button: {
+    primary: {
+      backgroundColor: "#008080",
+      color: "#FFFFFF",
+    },
+    secondary: {
+      backgroundColor: "#33915F",
+      color: "#FFFFFF",
+    },
+  },
+
+  // Page Backgrounds
+  pageBackground: {
+    default: "#191970",
+    secondary: "#008080",
+    tertiary: "#33915F",
+  },
+
+  // Icon Sizes
+  iconSize: {
+    small: 24,
+    medium: 32,
+    large: 48,
+  },
+
+  // Font Families
+  fontFamily: {
+    // ... (your font families here)
+  },
+
+  // Images
+  images: {
+    // logo: require("../assets/logo-no-background.png"),
+    // back: require("../assets/back.png"),
+  },
+};
+
+export default theme;
